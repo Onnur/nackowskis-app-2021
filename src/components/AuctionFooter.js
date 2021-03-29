db23b907d27daa3d2dd63ce002fd520963ffd965
Onnur <72163294+Onnur@users.noreply.github.com>
2021-03-29 21:36:11 +0200

    footer

--- src/components/AuctionFooter.js
+++ src/components/AuctionFooter.js
@@ -1,1 +1,19 @@
 import React from 'react';
+
+const AuctionFooter = () => {
+
+        return (
+            <div className="footerContent">
+                <div className="footer">
+                    <div className="content">
+                    <h1>Onur</h1>
+                    <h1>Anna-Maria</h1>
+                    <h1>Jakob</h1>
+                    <h1>Jimmy</h1>
+                    </div>   
+                </div>
+            </div>
+        );   
+        }
+
+        export default AuctionFooter;
