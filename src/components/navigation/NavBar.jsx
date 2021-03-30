88b189407a93c253c3d93e29c02f1272efb61c7e
Onnur <72163294+Onnur@users.noreply.github.com>
2021-03-30 12:24:19 +0200

    navbar1

--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -2,14 +2,51 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 //import '../../styles/AppStyles.css';
 
-const NavBar = () => {
+export default class Navbar extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {  };
+        this.onClick = this.onClick.bind(this);
+        this.handleEnterClick = this.handleEnterClick.bind(this);
+    }
 
-    return (<nav>
-                <ul>
-                    <li><NavLink to="/">Start</NavLink></li>
-                    <li><NavLink to="/add-auction">Lägg till auktion</NavLink></li>
-                </ul>
-            </nav>)
+    onClick(e){
+        let search = document.getElementById('searchField');
+        this.props.onChange(search.value);
+    }
+
+    handleEnterClick(event){
+        console.log(event.keyCode);
+        if (event.keyCode === 13){
+            document.getElementById('searchButton').click();
+        }
+    }
+
+    render(){
+        return (
+            <div className="navigation">
+                <div className="logo">
+                    <NavLink className="navbarLink" to="/">
+                        <h1 className="logoText">Nackowski: Grupp 5</h1>
+                    </NavLink>
+                </div>
+                <div className="searchBar">
+                    <input id="searchField" onKeyDown={this.handleEnterClick} placeholder="Sök auktion..." />
+                    <NavLink to="/Search">
+                        <button id="searchButton" aria-label="Search" onClick={this.onClick}><span aria-label="Search" role="img">&#128270;</span></button>
+                    </NavLink>
+                </div>
+                <div className="pageNavigation">
+                    <NavLink className="navbarLink" to="/">
+                        <h2>Startsida</h2>
+                    </NavLink>
+                </div>
+                <div className="pageNavigation">
+                    <NavLink className="navbarLink" to="/CreateAuction">
+                        <h2>Skapa auktion</h2>
+                    </NavLink>
+                </div>
+            </div>
+        );
+    }
 }
-
-export default NavBar ;
