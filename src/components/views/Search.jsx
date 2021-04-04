505dca1bf56508f7132a2e0f318aafdb0972d6b7
Jakob Silvmark <gerry496@gmail.com>
2021-04-04 21:06:11 +0200

    Rudimentär sökfunktion

---
+++ src/components/views/Search.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+
+
+<Route path="/Search">
+  <Search />
+</Route>
+
+const Search = () => {
+
+    return (
+        <div>
+            <h1>HEJ</h1>
+        </div>
+    )
+}
+
+export default Search
