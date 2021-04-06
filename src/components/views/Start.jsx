e81a4d7293b85f52a6bd722006121718538a9f4e
Onnur <72163294+Onnur@users.noreply.github.com>
2021-04-06 12:27:49 +0200

    Ändringar på start page

--- src/components/views/Start.jsx
+++ src/components/views/Start.jsx
@@ -16,15 +16,17 @@ const Start = () => {
             <br />
             {auctions.map(item =>
                 <NavLink to="/auctiondetails">
-                    <div className="auktion"
+                    <div className="foundAuctions"
                         value={item}
                         key={item.AuktionID}
                         onClick={(e) => {
                             setSelectedAuction(item)
                         }}
                     >
-                        <p>{item.Titel}</p>
-                        <p>{item.Beskrivning}</p>
+                        <h2>{item.Titel}</h2>
+                        <p>Slutar: {item.SlutDatum.replace('T', ' ')}</p>
+                        <h5>Utropspris: {item.Utropspris}</h5>
+                        <h5>Beskrivning: {item.Beskrivning}</h5>
                     </div>
                 </NavLink>
             )
