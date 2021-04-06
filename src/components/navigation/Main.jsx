02cc3329226fb96d55a110d37215e57ca56e82f2
Jakob Silvmark <gerry496@gmail.com>
2021-04-06 13:20:38 +0200

    sync

--- src/components/navigation/Main.jsx
+++ src/components/navigation/Main.jsx
@@ -2,24 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Start from '../views/Start';
 import AddAuction from '../views/AddAuction';
-<<<<<<< HEAD
-import Search from '../views/Search';
-=======
 import AuctionDetails from '../views/AuctionDetailView'
->>>>>>> 042abffb372cb58c6f99c365c7cacb431ea31ee7
 
 const Main = () => {
     return (
         <Switch>
-<<<<<<< HEAD
-            <Route exact path ="/" component={Start} />
-            <Route path ="/Search" component={Search} />
-            <Route path ="/add-auction" component={AddAuction} />
-=======
             <Route exact path="/" component={Start} />
             <Route path="/AddAuction" component={AddAuction} />
             <Route path="/auctiondetails" component={AuctionDetails} />
->>>>>>> 042abffb372cb58c6f99c365c7cacb431ea31ee7
         </Switch>
     );
 }
--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -2,35 +2,10 @@ import React, { useContext }from 'react';
 import { NavLink } from 'react-router-dom';
 import { AuctionContext } from '../../contexts/AuctionContextProvider'
 
-<<<<<<< HEAD
-export default class Navbar extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {searchTerm:""};
-        this.onClick = this.onClick.bind(this);
-        this.handleEnterClick = this.handleEnterClick.bind(this);
-    }
-
-    onClick(e){
-        let search = document.getElementById('searchField').value;
-        console.log("sökte "+this.state.searchTerm);
-        //this.props.onChange(search.value);
-    }
-
-    handleEnterClick(event){
-        console.log(event.keyCode);
-        //console.log('test2');
-        this.setState({searchTerm:document.getElementById('searchField').value});
-        if (event.keyCode === 13){
-            document.getElementById('searchButton').click();
-        }
-    }
-=======
 
 const NavBar = () => {
 
     const { auctions, setSelectedAuction, search, setSearchVal } = useContext(AuctionContext)
->>>>>>> 042abffb372cb58c6f99c365c7cacb431ea31ee7
 
         return (
             <div className="navigation">
@@ -39,25 +14,12 @@ const NavBar = () => {
                         <h1 className="logoText">Nackowski: Grupp 5</h1>
                     </NavLink>
                 </div>
-<<<<<<< HEAD
-                <div className="searchBar">
-                    <input id="searchField" onKeyDown={this.handleEnterClick} placeholder="Sök auktion..." />
-
-                    <NavLink to={{
-                    pathname:'/Search',
-                    state: {searchTerm:this.state.searchTerm}  
-                    }}>
-                    <button id="searchButton" aria-label="Search" onClick={this.onClick}><span aria-label="Search" role="img">&#128270;</span></button>
-                    </NavLink>
-
-=======
                 <div className="searchBar">               
                 <input type="text"
                 onChange={(e) => {
                     setSearchVal(e.target.value)
                 }} />
             <button onClick={search} role="img"><span aria-label="Search" role="img">&#128270;</span>sök</button>
->>>>>>> 042abffb372cb58c6f99c365c7cacb431ea31ee7
                 </div>
                 <div className="pageNavigation">
                     <NavLink className="navbarLink" to="/">
