811bb454e9d7061f8f67647f4ce248784e4480cd
Anna-Maria Phalén <anna-maria.phalen@outlook.com>
2021-04-11 22:20:48 +0200

    Updaterat uppdatera-funktion

--- src/components/views/UpdateAuction.jsx
+++ src/components/views/UpdateAuction.jsx
@@ -5,36 +5,38 @@ const UpdateAuction = () => {
 
     const { selectedAuction, setTitle, setDescription, setEndDate, updateAuction } = useContext(AuctionContext)
 
-    return (<div className="newAuction">
+    const defaultDate = selectedAuction.SlutDatum.toString().split('T', 10);
 
-        <h3>Ändra auktion</h3>
-        
-        <div className="title">
-            <label html for="title">Titel:</label><br />
-            <input type="text" placeholder = {selectedAuction.Titel} onChange={(e) => {
-                setTitle(e.target.value)
-            }} />
-        </div>
-        <div className="description">
-            <label html for="description">Beskrivning:</label><br />
-            <input type="text" value={selectedAuction.Beskrivning} onChange={(e) => {
-                setDescription(e.target.value)
-            }} /> 
-        </div>
-        <div className="dueDate">
-            <label html for="dueDate">Slutdatum:</label><br />
-            <input type="date" onChange={(e) => {
-                setEndDate(e.target.value)
-            }} />
-        </div>
-        <br />
-        <br />
-        <br />
-        <br />
-        <div className="createAuction">
-            <button className="createAuctionbtn" type="submit" onClick={updateAuction}>Ändra</button>
-        </div>
-    </div>)
+    return (<div className="newAuction">
+            
+                <h3>Ändra auktion</h3>
+                
+                <div className="title">
+                    <label html for="title">Titel:</label><br />
+                    <input type="text" defaultValue={selectedAuction.Titel} onChange={(e) => {
+                        setTitle(e.target.value)
+                    }} />
+                </div>
+                <div className="description">
+                    <label html for="description">Beskrivning:</label><br />
+                    <input type="text" defaultValue={selectedAuction.Beskrivning} onChange={(e) => {
+                        setDescription(e.target.value)
+                    }} /> 
+                </div>
+                <div className="dueDate">
+                    <label html for="dueDate">Slutdatum:</label><br />
+                    <input type="date" defaultValue={defaultDate[0]} onChange={(e) => {
+                        setEndDate(e.target.value)
+                    }} />
+                </div>
+                <br />
+                <br />
+                <br />
+                <br />
+                <div className="createAuction">
+                    <button className="createAuctionbtn" type="submit" onClick={updateAuction}>Ändra</button>
+                </div>
+            </div>)
 }
 
 export default UpdateAuction;
